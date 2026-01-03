076fc0ea1af9661788755fc0f6f34412fc835556
Kingasian <andrewwangpa@gmail.com>
2026-01-03 11:17:52 -0500

    Update dotenv to v16.6.0 and improve DB log message
    Upgraded the 'dotenv' package from version 16.4.5 to 16.6.0. Also updated the database connection log message to default NODE_ENV to 'development' if not set, and removed the display of the MongoDB URI for improved security.

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -44,5 +44,5 @@ export const connectToDatabase = async () => {
         throw err;
     }
 
-    console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
+    console.log(`Connected to database (${process.env.NODE_ENV || 'development'})`);
 }
